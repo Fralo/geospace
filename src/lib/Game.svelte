40df0d3056d2dc27541e0fa78afb2cffae343888
Fralo <leonardo.fralini@gmail.com>
2023-03-15 20:49:46 +0100

    style: :lipstick: add new defulat font

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -1,32 +1,141 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import "@tensorflow/tfjs";
+    import * as poseDetection from "@tensorflow-models/pose-detection";
+    import { Circle, detectCollision, Target } from "../game/Shapes";
 
-    export let width = 800;
-    export let height = 600;
+    let video = null;
+    let canvas: HTMLCanvasElement = null;
+    let ctx: CanvasRenderingContext2D = null;
+    let detector = null;
 
+    const drawVideo = true;
 
-
-    let canvas: HTMLCanvasElement;
-    let gl: CanvasRenderingContext2D;
-
-    function repeatOften() {
-        // Do whatever
-        requestAnimationFrame(repeatOften);
-    }
-
-    onMount(() => {
-        canvas = document.getElementById("game") as HTMLCanvasElement;
-        gl = canvas.getContext("2d");
-
-        requestAnimationFrame(repeatOften);
-        drwaRect(100, 100, 50, 50);
-    });
-
-    const drwaRect = (x, y, w, h) => {
-        console.log(gl)
-        gl.fillStyle = "#FF0000";
-        gl.fillRect(x, y, w, h);
+    const MODES = {
+        MODE_HANDS: ["right_wrist", "left_wrist"],
+        MODE_NOSE: ["nose"],
     };
+
+    const selectedMode = "MODE_NOSE";
+
+    const videoWidth = window.innerWidth / 2.8;
+    const videoHeight = window.innerWidth / 2.8;
+
+    let target = new Target();
+    let score = 0;
+
+    const setupCamera = async () => {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+            throw new Error(
+                "Browser API navigator.mediaDevices.getUserMedia not available"
+            );
+        }
+
+        console.log(videoWidth, videoHeight);
+        video.width = videoWidth;
+        video.height = videoHeight;
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                facingMode: "user",
+                width: videoWidth,
+                height: videoHeight,
+            },
+        });
+
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+            video.play();
+        };
+
+        setupCanvas();
+    };
+
+    const setupCanvas = () => {
+        target.setRandomCoords(canvas);
+        canvasLoop();
+    };
+
+    const canvasLoop = async () => {
+        ctx.save();
+
+        ctx.translate(canvas.width, 0);
+        ctx.scale(-1, 1);
+
+        const estimationConfig = {
+            maxPoses: 1,
+            flipHorizontal: false,
+            scoreThreshold: 0.5,
+            nmsRadius: 20,
+        };
+
+        const poses = await detector.estimatePoses(canvas, estimationConfig);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+
+        let circles = [];
+
+        let keypoints = getKeypoints(poses);
+        keypoints.forEach(({ x, y, score }) => {
+            if (score > 0.2) {
+                let circle = new Circle(20);
+                circle.setCoords(canvas.width - x, y);
+                circle.draw(canvas, ctx);
+                circles.push(circle);
+            }
+        });
+
+        target.draw(canvas, ctx);
+
+        circles.forEach((circle) => {
+            if (detectCollision(circle, target)) {
+                target.setRandomCoords(canvas);
+                score++;
+            }
+        });
+
+        ctx.restore();
+        requestAnimationFrame(canvasLoop);
+    };
+
+    const getKeypoints = (poses) => {
+        if (!poses || !poses.length) return [];
+        let allKeypoints = poses[0].keypoints;
+
+        return allKeypoints.filter((keypoint) => {
+            return MODES[selectedMode].includes(keypoint.name);
+        });
+    };
+
+    const initializePosenet = async () => {
+        const detectorConfig = {
+            modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+        };
+        const model = poseDetection.SupportedModels.MoveNet;
+        detector = await poseDetection.createDetector(model, detectorConfig);
+    };
+
+    onMount(async () => {
+        video = document.getElementById("video") as HTMLVideoElement;
+        canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+        canvas.width = videoWidth;
+        canvas.height = videoHeight;
+        ctx = canvas.getContext("2d");
+
+        await initializePosenet();
+
+        setupCamera();
+    });
 </script>
 
-<canvas id="game" {width} {height} class="border-white border-8" />
+<div>
+    <div class="">
+        <video class="hidden" id="video" playsinline>
+            Video stream not available.
+        </video>
+        <canvas id="canvas" />
+    </div>
+</div>
