c59aa1405691d0ab4a4dca3d7b266e7906b4bcee
Fralo <leonardo.fralini@gmail.com>
2023-03-21 09:58:06 +0100

    feat: :sparkles: update button

---
+++ src/lib/common/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let type = "primary";
+
+    let classes = "px-5 py-3 text-base font-medium rounded-md active:translate-y-[1px] transition-all ";
+
+    switch (type) {
+        case "secondary":
+            classes += "text-white";
+            break;
+        case "primary":
+        default:
+            classes += "bg-white text-gray-900 hover:bg-gray-200";
+    }
+</script>
+
+<button class={classes}>
+    <slot />
+</button>
