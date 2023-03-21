0b86c5933b4096103f2dde0c3e4a508a4f2d52df
Fralo <leonardo.fralini@gmail.com>
2023-03-21 09:58:13 +0100

    update home

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,8 +1,23 @@
 <script>
+    import Button from "../lib/common/Button.svelte";
 </script>
 
 <div class="h-full">
     <div class="flex items-center min-h-full justify-center text-5xl font-bold">
-        HOME
+        <h1>GEOSPACE</h1>
+        <div>
+            Geospace è un coinvolgente web-game di riflessi che mette alla prova
+            la velocità e l'abilità dei giocatori. Ambientato in uno spazio
+            futuristico, il gioco presenta un'interfaccia grafica accattivante e
+            una colonna sonora elettronica che contribuiscono a creare
+            un'atmosfera coinvolgente. L'obiettivo del gioco è di evitare
+            ostacoli e pericoli che si presentano lungo il percorso e
+            raccogliere il maggior numero possibile di power-up per aumentare il
+            punteggio. I giocatori controllano un'astronave che si muove ad alta
+            velocità e deve evitare gli ostacoli saltando o abbassandosi, a
+            seconda della loro posizione.
+        </div>
+
+        <Button type="primary">Sono un bottone</Button>
     </div>
 </div>
