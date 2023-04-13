764a6abf07b87e4d852bcfcd39fdd703a9babf7a
Fralo <leonardo.fralini@gmail.com>
2023-04-13 22:05:12 +0200

    feat: :sparkles: add new setup layout

--- src/lib/common/Button.svelte
+++ src/lib/common/Button.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     export let type = "primary";
+    export let otherClasses = "";
 
-    let classes = "px-5 py-3 text-base font-medium rounded-md active:translate-y-[1px] transition-all ";
+    let classes =
+        `px-5 py-3 text-base font-medium rounded-md active:translate-y-[1px] transition-all ${otherClasses}`;
 
     switch (type) {
         case "secondary":
-            classes += "text-white";
+            classes = `${classes} text-white`;
             break;
         case "primary":
         default:
-            classes += "bg-white text-gray-900 hover:bg-gray-200";
+            classes = `${classes} bg-white text-gray-900 hover:bg-gray-200`;
     }
 </script>
 
--- src/lib/training/Setup.svelte
+++ src/lib/training/Setup.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import { createEventDispatcher } from "svelte";
 
     const dispatch = createEventDispatcher();
@@ -52,24 +52,34 @@
     };
 </script>
 
-<div>
-    <h2>Seleziona le impostazioni di allenamento</h2>
+<div class="mt-28 h-full flex justify-center items-center">
+    <div class="min-w-[450px] p-6 py-10 border-2 border-white rounded-3xl">
+        <div>
+            <h1 class="text-4xl font-bold">SETUP</h1>
+            <h3 class="text-2xl font-semibold">
+                Choose the game settings
+            </h3>
+        </div>
 
-    <div>
-        <div>
-            <span>Seleziona il tempo: </span>
-            <NumberInput bind:value={time} />
-            <Select bind:value={timeMultilpier} selectOptions={timeUnits} />
+        <div class="mt-8 flex flex-col gap-4">
+            <div class="flex items-center gap-2">
+                <span>Select the time:</span>
+                <NumberInput bind:value={time} />
+                <Select bind:value={timeMultilpier} selectOptions={timeUnits} />
+            </div>
+            <div class="flex items-center gap-2">
+                <span>Select the game mode:</span>
+                <Select bind:value={mode} selectOptions={modes} />
+            </div>
+            <div class="flex items-center gap-2">
+                <span>Select the number of players</span>
+                <Select bind:value={playersNum} selectOptions={playesOption} />
+            </div>
         </div>
-        <div>
-            <span>Seleziona la modalità di gioco</span>
-            <Select bind:value={mode} selectOptions={modes} />
-        </div>
-        <div>
-            <span>Seleziona il numero di giocatori</span>
-            <Select bind:value={playersNum} selectOptions={playesOption} />
+        <div class="mt-20 flex justify-center">
+            <Button otherClasses="w-full" on:click={startTraining}
+                >Avvia</Button
+            >
         </div>
     </div>
-
-    <Button on:click={startTraining}>Avvia</Button>
 </div>
