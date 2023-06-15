b66a5be6da9175feede1e63348195cdcefbca053
Fralo <leonardo.fralini@gmail.com>
2023-06-15 19:56:12 +0200

    feat: :sparkles: mior updates

--- src/lib/game-stats/OnePlayerGameStats.svelte
+++ src/lib/game-stats/OnePlayerGameStats.svelte
@@ -9,6 +9,8 @@
         ttfh: 0,
     };
 
+    export let customClass = null;
+
     let formattedStats = {
         score: stats.score,
         time: stats.time,
@@ -27,28 +29,42 @@
         },
         {
             label: "Medium speed",
-            value: `${formattedStats.mediumSpeed} hits/second`,
+            value: `${new Intl.NumberFormat("en-IN", {
+                maximumSignificantDigits: 2,
+            }).format(formattedStats.mediumSpeed)} hits/second`,
         },
         {
             label: "TTFH (Time To First Hit)",
-            value: formattedStats.ttfh ? `${formattedStats.ttfh} milliseconds` : 'N/A',
+            value: formattedStats.ttfh
+                ? `${formattedStats.ttfh} milliseconds`
+                : "N/A",
         },
     ];
 </script>
 
-<div class="border border-white py-4 rounded-3xl w-fit">
+<div
+    class={`relative border border-white py-4 rounded-3xl max-w-lg ${
+        customClass || ""
+    }`}
+>
+    {#if winning}
+        <div class="absolute -top-5 -left-5">
+            <TrophyCupIcon width={44} height={44} fill="white" />
+        </div>
+    {/if}
     <div
         class="border-b border-white text-center font-bold tracking-widest text-4xl pb-4"
     >
-        {#if !winning}
-            <TrophyCupIcon fill="white"/>
-        {/if}
         <p>
             {title}
         </p>
     </div>
-    {#each toShowStats as stat}
-        <div class="mx-4 border-b border-white last:border-b-0">
+    {#each toShowStats as stat, index}
+        <div
+            class={`mx-4 border-b border-white last:border-b-0 ${
+                index === 0 ? "flex gap-3 items-center" : ""
+            }`}
+        >
             <div class="p-2 text-2xl">{stat.label}:</div>
             <div class="p-2 text-4xl">{stat.value}</div>
         </div>
--- src/lib/game-stats/TwoPlayersStats.svelte
+++ src/lib/game-stats/TwoPlayersStats.svelte
@@ -3,28 +3,49 @@
 
     export let stats = [];
 
-
     let winner =
         stats[0].score > stats[1].score
-            ? "Player 1"
+            ? -1
             : stats[0].score === stats[1].score
-            ? "Nobody"
-            : "Player 2";
+            ? 0
+            : 1;
+
+    let winnerLabel = "";
+    switch (true) {
+        case winner === -1:
+            winnerLabel = "Player 1";
+            break;
+        case winner === 0:
+            winnerLabel = "Nobody";
+            break;
+        case winner === 1:
+            winnerLabel = "Player 2";
+            break;
+    }
 </script>
 
 <div class="w-full flex items-center justify-center">
     <div>
         <div>
             <h3>Results:</h3>
-            <h1>{winner} wins !</h1>
+            <h1>{winnerLabel} wins !</h1>
         </div>
 
         <div>Here are some statistics about the game</div>
 
-        <div class="mt-10 flex gap-10">
-
-            <OnePlayerGameStats title="Player 1 Stats" stats={stats[0]} />
-            <OnePlayerGameStats title="Player 2 Stats" stats={stats[1]} />
+        <div class="mt-10 grid grid-cols-2 gap-10">
+            <OnePlayerGameStats
+                customClass="col-span-1"
+                title="Player 1 Stats"
+                stats={stats[0]}
+                winning={winner === -1}
+            />
+            <OnePlayerGameStats
+                customClass="col-span-1"
+                title="Player 2 Stats"
+                stats={stats[1]}
+                winning={winner === 1}
+            />
         </div>
     </div>
 </div>
